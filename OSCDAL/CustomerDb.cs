b20b0303e4e4526c980c098e05f0a09a085bb24b
MathewNhari <49167930+MathewNhari@users.noreply.github.com>
2020-04-01 23:43:39 +0200

    Add files via upload

---
+++ OSCDAL/CustomerDb.cs
@@ -0,0 +1,43 @@
+﻿using OSCBOL;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OSCDAL
+{
+    public class CustomerDb
+    {
+        OSCDbContext OSCDbContext;
+        public CustomerDb()
+        {
+            OSCDbContext = new OSCDbContext();
+        }
+
+        public IEnumerable<Customer> GetAll()
+        {
+            IEnumerable<Customer> customers = OSCDbContext.Customers.ToList();
+            return customers;
+        }
+        public Customer GetById(int id)
+        {
+            Customer customer = OSCDbContext.Customers.Find(id);
+            return customer;
+        }
+        public void Add(Customer customer)
+        {
+            OSCDbContext.Customers.Add(customer);
+
+        }
+        public void Update(Customer customer)
+        {
+            OSCDbContext.Customers.Update(customer);
+        }
+        public void Delete(Customer customer)
+        {
+            OSCDbContext.Customers.Remove(customer);
+        }
+        public void Save()
+        {
+            OSCDbContext.SaveChanges();
+        }
+    }
+}
---
+++ OSCDAL/Migrations/20200305075605_InitialDb.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+
+namespace OSCDAL.Migrations
+{
+    public partial class InitialDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customer",
+                columns: table => new
+                {
+                    CustomerId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CustomerName = table.Column<string>(nullable: false),
+                    CustomerEmail = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customer", x => x.CustomerId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Product",
+                columns: table => new
+                {
+                    ProductId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProductName = table.Column<string>(nullable: false),
+                    PricePerUnit = table.Column<double>(nullable: false),
+                    Stock = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product", x => x.ProductId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Order",
+                columns: table => new
+                {
+                    OrderId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CustomerId = table.Column<int>(nullable: false),
+                    ProductId = table.Column<int>(nullable: false),
+                    Quantity = table.Column<int>(nullable: false),
+                    DateOfOrder = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Order", x => x.OrderId);
+                    table.ForeignKey(
+                        name: "FK_Order_Customer_CustomerId",
+                        column: x => x.CustomerId,
+                        principalTable: "Customer",
+                        principalColumn: "CustomerId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Order_Product_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Product",
+                        principalColumn: "ProductId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Order_CustomerId",
+                table: "Order",
+                column: "CustomerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Order_ProductId",
+                table: "Order",
+                column: "ProductId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Order");
+
+            migrationBuilder.DropTable(
+                name: "Customer");
+
+            migrationBuilder.DropTable(
+                name: "Product");
+        }
+    }
+}
