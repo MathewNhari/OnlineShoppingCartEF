7237b8473618206a12a2f645b650cb2b1cad1c04
MathewNhari <49167930+MathewNhari@users.noreply.github.com>
2020-04-01 23:40:32 +0200

    Add files via upload

---
+++ OSCBLL/CustomerBs.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OSCDAL;
+using OSCBOL;
+
+namespace OSCBLL
+{
+    class CustomerBs
+    {
+         
+        CustomerDb CustomerDb;
+        public CustomerBs()
+        {
+            CustomerDb = new CustomerDb();
+        }
+        public IEnumerable<Customer> GetAll()
+        {
+            return CustomerDb.GetAll();
+        }
+        public Customer GetById(int id)
+        {
+            return CustomerDb.GetById(id);
+        }
+        public void Add(Customer customer)
+        {
+            CustomerDb.Add(customer);
+        }
+        public void Update(Customer customer)
+        {
+            CustomerDb.Update(customer);
+        }
+        public void Delete(Customer customer)
+        {
+            CustomerDb.Delete(customer);
+        }
+        public void Save()
+        {
+            CustomerDb.Save();
+        }
+    }
+}
+
